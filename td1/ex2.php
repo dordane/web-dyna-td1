11b3db98fba798de93ba63ecc93e7cfe99c22980
Doyorn <Doyorn@outlook.fr>
2020-03-18 15:40:11 +0100

    Exo 2

--- td1/ex2.php
+++ td1/ex2.php
@@ -1,33 +1,38 @@
-      <!DOCTYPE html>
-      <html>
-      <head>
-          <meta charset="UTF-8">
-          <title>Hello</title>
-      </head>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Hello</title>
+</head>
 
-      <!--
-      Pour obtenir des couleurs facilement
+<!--
+Pour obtenir des couleurs facilement
 
-      <form method="GET">
-          <label for="color">Color : </label>
-          <input type="color" id="html5colorpicker" onchange="clickColor(0, -1, -1, 5)" value="#ff0000" name="Color">
-          <label for="size">Size : </label>
-          <input type="number" value="" name="size" id="size">
-          <input type="submit" value="Valider">
-      </form>
-      -->
+<form method="GET">
+<label for="color">Color : </label>
+<input type="color" id="html5colorpicker" onchange="clickColor(0, -1, -1, 5)" value="#ff0000" name="Color">
+<label for="size">Size : </label>
+<input type="number" value="" name="size" id="size">
+<input type="submit" value="Valider">
+</form>
+-->
 
-      <body>
-      <a href="?Color=%23ff0000&size=15&message=Bonjour">Message 1</a>
-      <a href="?Color=%2339ff00&size=30&message=ça">Message 2</a>
-      <a href="?Color=%230080ff&size=50&message=Marche">Message 3</a>
-          <?php
-          if (empty($_GET)) {
-          echo "Erreur ! Veuillez entrer des paramètres";
-      }
-      else{
-          echo "<div style='font-size: {$_GET["size"]}px; color: {$_GET["Color"]}'>{$_GET["message"]}</div>";
-          }
-          ?>
-      </body>
-      </html>
+<body>
+  <a href="?Color=%23ff0000&size=15&message=Bonjour">Message 1</a>
+  <a href="?Color=%2339ff00&size=30&message=ça">Message 2</a>
+  <a href="?Color=%230080ff&size=50&message=Marche">Message 3</a>
+  <a href="?&message=Sans couleurs">Sans couleur sans size</a>
+  <?php
+  if (empty($_GET)) {
+    echo "Erreur ! Veuillez entrer des paramètres";
+  }
+  if (empty($_GET["size"] and $_GET["Color"])) {
+    echo "<div style='font-size: 120px; color: black'>{$_GET["message"]}</div>";
+  }
+  else {
+    echo "<div style='font-size: {$_GET["size"]}px; color: {$_GET["Color"]}'>{$_GET["message"]}</div>";
+  }
+
+  ?>
+</body>
+</html>
