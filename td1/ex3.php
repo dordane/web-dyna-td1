a160c41582255aeec64ba6a2d2b131840963ab45
Doyorn <Doyorn@outlook.fr>
2020-03-19 11:47:26 +0100

    sauvegarde

--- td1/ex3.php
+++ td1/ex3.php
@@ -5,9 +5,8 @@
   <title>Hello</title>
 </head>
 
-
 <body>
-<form method="POST">
+<form action="ex3.php" method="POST">
 <label for="color">Color : </label>
 <input type="color" id="html5colorpicker" onchange="clickColor(0, -1, -1, 5)" value="#ff0000" name="Color">
 <label for="size">Size : </label>
@@ -16,21 +15,19 @@
 <input type="text" value="" name="message" id="message">
 <input type="submit" value="Valider">
 </form>
-
-
   <a href="?Color=%23ff0000&size=15&message=Bonjour">Message 1</a>
   <a href="?Color=%2339ff00&size=30&message=ça">Message 2</a>
   <a href="?Color=%230080ff&size=50&message=Marche">Message 3</a>
   <a href="?&message=Sans couleurs">Sans couleur sans sizee</a>
   <?php
-  if (empty($_GET)) {
+  if (empty($_POST)) {
     echo "Erreur ! Veuillez entrer des paramètres";
   }
-  if (empty($_GET["size"] and $_GET["Color"])) {
-    echo "<div style='font-size: 12px; color: black'>{$_GET["message"]}</div>";
+  if (empty($_POST["size"] and $_POST["Color"])) {
+    echo "<div style='font-size: 12px; color: black'>{$_POST["message"]}</div>";
   }
   else {
-    echo "<div style='font-size: {$_GET["size"]}px; color: {$_GET["Color"]}'>{$_GET["message"]}</div>";
+    echo "<div style='font-size: {$_POST["size"]}px; color: {$_POST["Color"]}'>{$_POST["message"]}</div>";
   }
   ?>
 </body>
