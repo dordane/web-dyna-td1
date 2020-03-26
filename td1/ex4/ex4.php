9e9748582d9678c2e9aa737a567ef2afecc3b53d
Doyorn <Doyorn@outlook.fr>
2020-03-26 16:00:38 +0100

    sauvegarde

--- td1/ex4/ex4.php
+++ td1/ex4/ex4.php
@@ -7,27 +7,8 @@
 	<a href="?nblignes=2&nbColonnes=2">2x2</a>
 	<a href="?nblignes=5&nbColonnes=5">5x5</a>
 	<a href="?nblignes=10&nbColonnes=8">10x8</a>
-	<hr>
-	<hr>
-	<hr>
 
 	<?php
-	echo "<p>{$_GET["nbColonnes"]}-{$_GET["nblignes"]}</p>";
-	echo "<table border='1'>
-	<tbody>
-	<tr>
-	<td>1-1</td>
-	<td>1-2</td>
-	</tr>
-	<tr>
-	<td>2-1</td>
-	<td>2-2</td>
-	</tr>
-	</tbody>
-	</table>
-	";
-	echo "<hr>";
-
 	echo "<table border='1'>
 	<tbody>";
 
