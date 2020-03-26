d8d73ce8108a6dd1125b109d316d844eca848a60
Doyorn <Doyorn@outlook.fr>
2020-03-26 16:58:53 +0100

    Update ex4.php

--- td1/ex4/ex4.php
+++ td1/ex4/ex4.php
@@ -8,7 +8,7 @@
 	<a href="?nblignes=5&nbColonnes=5">5x5</a>
 	<a href="?nblignes=10&nbColonnes=8">10x8</a>
 
-	
+	<p style="color: red">aa</p>
 	<?php
 	echo "<table border='1'>
 	<tbody>";
@@ -22,7 +22,13 @@
 		}
 		echo "<tr>"; 
 		for($c=0;$c<=$_GET["nbColonnes"];$c++){ 
-			echo "<td align='center'><p style='font-weight: $gras;'>$l-$c</p></td>"; 
+			if (($l % 2) == 0){
+				$color = "red";
+			}
+			else{
+				$color = "black";
+			}
+			echo "<td align='center'><p style='font-weight: $gras; color: $color'>$l-$c</p></td>"; 
 		} 
 		echo "</tr>"; 
 	} 
