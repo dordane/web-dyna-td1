580363b4c90b00384aa99535fb4fcea18c89c97a
Doyorn <Doyorn@outlook.fr>
2020-03-26 14:20:21 +0100

    sauvegarde

---
+++ td1/ex4/ex4.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ex4</title>
+</head>
+<body>
+	<a href="?nblignes=2&nbColonnes=2">2x2</a>
+	<a href="?nblignes=2&nbColonnes=2">5x5</a>
+	<a href="?nblignes=2&nbColonnes=2">10x8</a>
+	<?php
+	echo "Erreur ! Veuillez entrer des paramètres";  
+	<table border='1'>
+	<tbody>
+	<tr>
+	<td>1-1</td>
+	<td>1-2</td>
+	</tr>
+	<tr>
+	<td>2-1</td>
+	<td>2-2</td>
+	</tr>
+	</tbody>
+	</table>
+	?>
+</body>
+</html>
