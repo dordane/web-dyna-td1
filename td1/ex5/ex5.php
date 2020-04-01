a381929b8614367e1917ce45077a26a00457759a
Doyorn <Doyorn@outlook.fr>
2020-04-01 09:31:29 +0200

    Update ex5.php

--- td1/ex5/ex5.php
+++ td1/ex5/ex5.php
@@ -4,11 +4,15 @@
 	<title></title>
 </head>
 <body>
-	<?php
-	include("functions.php");
-	createHtmlTable(3,5);
-	?>
-	
-
+	<form method="POST">
+		<label for="contenue">C*L</label>
+		<input id="colonnes" name="colonnes" placeholder="Colonnes"></input>
+		<input id="lignes" name="lignes" placeholder="Lignes"></input>
+	<input type="submit" value="Valider">
+</form>
+<?php
+include("functions.php");
+createHtmlTable($_POST["lignes"]-1,$_POST["colonnes"]-1);
+?>
 </body>
 </html>
