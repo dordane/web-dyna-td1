defb0b5fb3c70f1c2dff3e167817fbda0cbfdd8a
Doyorn <Doyorn@outlook.fr>
2020-05-12 15:28:16 +0200

    t

---
+++ tp4/app/cache/annotations/InscriptionController.php-354b32dd.annotations.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+  '#namespace' => 'controllers',
+  '#uses' => array (
+),
+  '#traitMethodOverrides' => array (
+  'controllers\\InscriptionController' => 
+  array (
+  ),
+),
+  'controllers\\InscriptionController::index' => array(
+    array('#name' => 'get', '#type' => 'Ubiquity\\annotations\\router\\GetAnnotation', "_default","name"=>"Home")
+  ),
+  'controllers\\InscriptionController::bouton' => array(
+    array('#name' => 'route', '#type' => 'Ubiquity\\annotations\\router\\RouteAnnotation', "inscription")
+  ),
+  'controllers\\InscriptionController::partnerDetails' => array(
+    array('#name' => 'route', '#type' => 'Ubiquity\\annotations\\router\\RouteAnnotation', "MainController/partner/{name}","methods"=>["get"])
+  ),
+  'controllers\\InscriptionController::get' => array(
+    array('#name' => 'route', '#type' => 'Ubiquity\\annotations\\router\\RouteAnnotation', "/do/inscriptiona","methods"=>["get"])
+  ),
+);
+
